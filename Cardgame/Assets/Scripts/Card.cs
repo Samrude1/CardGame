e9a84375b9ba30482b9f010b75c874c47f428c04
Samrude1 <126822763+Samrude1@users.noreply.github.com>
2023-08-30 20:49:52 +0300

    Drag'nDrop
    Korttei voi siirrellä käsin

--- Cardgame/Assets/Scripts/Card.cs
+++ Cardgame/Assets/Scripts/Card.cs
@@ -18,6 +18,10 @@ public class Card : MonoBehaviour
     public bool inHand;
     public int handPosition;
     public GameObject card;
+    public LayerMask desktopLayermask;
+    public LayerMask placementLayermask;
+    public float rayLength;
+    public CardPlacement assignedPlace;
 
     public Image cardImage;
     //public TMP_Text defenceText;
@@ -31,12 +35,16 @@ public class Card : MonoBehaviour
     private Quaternion targetRotation;
     private HandManager handM;
     private Vector3 scaleChange;
+    private bool isSelected;
+    private Collider2D cardCollider;
+    private bool justPressed;
 
     // Start is called before the first frame update
     void Start()
     {
         CardBuild();
         handM = FindObjectOfType<HandManager>();
+        cardCollider = GetComponent<Collider2D>();
     }
 
     public void CardBuild()
@@ -58,6 +66,54 @@ public class Card : MonoBehaviour
     {
         transform.position = Vector3.Lerp(transform.position, targetP, speed * Time.deltaTime);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        if(isSelected)
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = -Camera.main.transform.position.z; // Set the z-coordinate to match your camera's depth
+
+            Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+            RaycastHit2D hit2D = Physics2D.Raycast(ray.origin, ray.direction, rayLength, desktopLayermask);
+
+            if (hit2D.collider != null)
+            {
+                Debug.Log("RAY HITS");
+                MoveTo(new Vector3(hit2D.point.x, hit2D.point.y, transform.position.z), transform.rotation);
+            }
+            if(Input.GetMouseButtonDown(1))
+            {
+                ReturnToHand();
+            }
+            if(Input.GetMouseButtonDown(0) && justPressed == false)
+            {
+                Ray ray2 = Camera.main.ScreenPointToRay(mousePosition);
+
+                RaycastHit2D hit2D2 = Physics2D.Raycast(ray2.origin, ray2.direction, rayLength, placementLayermask);
+                if (hit2D2.collider != null) 
+                {
+                    Debug.Log("RAY HITS");
+                    CardPlacement selectedPoint = hit2D2.collider.GetComponent<CardPlacement>();
+                    if (selectedPoint.activeCard == null && selectedPoint.isPlayerPoint)
+                    {
+                        selectedPoint.activeCard = this;
+                        assignedPlace = selectedPoint;
+
+                        MoveTo(selectedPoint.transform.position, transform.rotation);
+                        inHand = false;
+                        isSelected = false;
+                    }
+                    else
+                    {
+                        ReturnToHand() ;
+                    }
+                }
+                else
+                {
+                    ReturnToHand();
+                }
+            }
+        }
+        justPressed = false;
     }
 
     public void MoveTo(Vector3 moveTo, Quaternion rotateTo)
@@ -71,10 +127,10 @@ public class Card : MonoBehaviour
         if(inHand)
         {
             currentPlace = transform.position;
-            Debug.Log("Touching...");
+            //Debug.Log("Touching...");
             // Gradually scale up the card to the target scale over 0.5 seconds
             StartCoroutine(ScaleCardOverTime(new Vector3(2.5f, 2.5f, 2.5f), 0.1f));
-            transform.position = currentPlace + new Vector3(0f, 0.01f, -0.05f);
+            transform.position = currentPlace + new Vector3(0f, 0.01f, -0.01f);
         }
     }
 
@@ -82,11 +138,11 @@ public class Card : MonoBehaviour
     {
         if(inHand)
         {
-            Debug.Log("NOT Touching...");
+            //Debug.Log("NOT Touching...");
             MoveTo(handM.cardPositions[handPosition], handM.leftBound.rotation);
             // Gradually scale down the card to the original scale over 0.5 seconds
             StartCoroutine(ScaleCardOverTime(new Vector3(1f, 1f, 1f), 0.5f));
-            
+            transform.position = currentPlace + new Vector3(0f, 0.01f, 1.01f);
         }
     }
 
@@ -103,5 +159,24 @@ public class Card : MonoBehaviour
         }
 
         card.transform.localScale = targetScale;
+    }
+
+    private void OnMouseDown()
+    {
+        if(inHand)
+        {
+            isSelected = true;
+            cardCollider.enabled = false;
+            justPressed = true;
+        }
+        
+    }
+
+    public void ReturnToHand()
+    {
+        isSelected = false;
+        cardCollider.enabled = true;
+
+        MoveTo(handM.cardPositions[handPosition], handM.leftBound.rotation);
     }
 }
---
+++ Cardgame/Assets/Scripts/CardPlacement.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardPlacement : MonoBehaviour
+{
+    public Card activeCard;
+    public bool isPlayerPoint;
+}
